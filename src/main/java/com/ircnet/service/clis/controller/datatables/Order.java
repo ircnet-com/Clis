63a48b177d6d992de82eab804219a9b1410f9050
Patrick Okraku <p.okraku@babiel.com>
2019-11-23 14:24:11 +0100

    .

---
+++ src/main/java/com/ircnet/service/clis/controller/datatables/Order.java
@@ -0,0 +1,41 @@
+package com.ircnet.service.clis.controller.datatables;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+/**
+ * https://datatables.net/manual/server-side
+ */
+public class Order {
+  /**
+   * Column to which ordering should be applied. This is an index reference to the columns array of information that is
+   * also submitted to the server.
+   */
+  private Integer column;
+
+  /**
+   * Ordering direction for this column. It will be asc or desc to indicate ascending ordering or descending ordering,
+   * respectively.
+   */
+  private String dir;
+
+  public Order() {
+  }
+
+  public Integer getColumn() {
+    return column;
+  }
+
+  public void setColumn(Integer column) {
+    this.column = column;
+  }
+
+  public String getDir() {
+    return dir;
+  }
+
+  public void setDir(String dir) {
+    this.dir = dir;
+  }
+}
