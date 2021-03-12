54e38dc9de18dc884f4681add06018704b935d2e
Patrick <patrick@ircnet.com>
2021-03-13 00:14:30 +0100

    Enabled lag check

--- src/main/java/com/ircnet/service/clis/event/YouAreServiceEventListener.java
+++ src/main/java/com/ircnet/service/clis/event/YouAreServiceEventListener.java
@@ -1,10 +1,15 @@
 package com.ircnet.service.clis.event;
 
+import com.ircnet.library.common.SettingConstants;
+import com.ircnet.library.common.SettingService;
 import com.ircnet.library.common.event.AbstractEventListener;
 import com.ircnet.library.service.event.YouAreServiceEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.Date;
 
 /**
  * Event for YOURESERVICE (383) message:
@@ -18,7 +23,13 @@ import org.springframework.stereotype.Component;
 public class YouAreServiceEventListener extends AbstractEventListener<YouAreServiceEvent> {
     private static final Logger LOGGER = LoggerFactory.getLogger(YouAreServiceEventListener.class);
 
+    @Autowired
+    private SettingService settingService;
+
     protected void onEvent(YouAreServiceEvent event) {
         LOGGER.info("Service connected as {}", event.getServiceName());
+
+        if(settingService.findInteger(SettingConstants.LAGCHECK_INTERVAL, SettingConstants.LAGCHECK_INTERVAL_DEFAULT) != 0)
+            event.getIRCConnection().setLagCheckNext(new Date(System.currentTimeMillis() + 120 * 1000));
     }
 }
